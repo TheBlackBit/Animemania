1156fd9a380da34ec4d7d264ec33f42a2783834d
Ever Solis <eversolis@theblackbit.io>
2023-08-13 01:59:48 -0600

    Class name renamed

--- core/data/src/main/java/com/theblackbit/animemania/android/data/external/datasource/categories/AnimeCategories.kt
+++ core/data/src/main/java/com/theblackbit/animemania/android/data/external/datasource/categories/AnimeCategories.kt
@@ -5,22 +5,22 @@ const val MOST_ANTICIPATED_ANIME_ID = 2
 const val TOP_RATED_ANIME_ID = 3
 const val POPULAR_ANIME_ID = 4
 val animeCategories = listOf(
-    Categories(
+    CategoryData(
         id = TRENDING_ANIME_ID,
         name = "Trending",
         collectionType = CollectionType.ANIME,
     ),
-    Categories(
+    CategoryData(
         id = MOST_ANTICIPATED_ANIME_ID,
         name = "Most Anticipated",
         collectionType = CollectionType.ANIME,
     ),
-    Categories(
+    CategoryData(
         id = TOP_RATED_ANIME_ID,
         name = "Top Rated",
         collectionType = CollectionType.ANIME,
     ),
-    Categories(
+    CategoryData(
         id = POPULAR_ANIME_ID,
         name = "Popular",
         collectionType = CollectionType.ANIME,
